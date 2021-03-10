e0b2e7587c8d998955e373532350214211317bf8
celilreha <celilrehaesgice@gmail.com>
2021-03-10 20:22:27 +0300

    Resources

---
+++ Resources_Kavramlari/app/src/main/java/com/celilreha/resources_kavramlari/MainActivity.java
@@ -0,0 +1,14 @@
+package com.celilreha.resources_kavramlari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
